9559daf2e89810cf4ce243cf6ba41f476bc81bb4
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-07-26 09:52:33 -0700

    add unique key to li items

--- src/book.js
+++ src/book.js
@@ -1,29 +1,27 @@
 import React, { Component } from 'react';
 
 class Book extends Component {
-	// constructor(props) {
-	//     super(props);
-	//     this.selectShelf = this.selectShelf.bind(this);
-	//   }
-
 	render() {
+		const { res, bookItem, selectShelf } = this.props;
 		return (
-			<div className="book">
-              <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url('+ this.props.res.imageLinks.smallThumbnail +')' }}></div>
-                <div className="book-shelf-changer">
-                  <select onChange={(e, r) => this.props.selectShelf(e.target.value, this.props.bookItem)}>
-                    <option value="none">Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-title">{this.props.res.title}</div>
-              <div className="book-authors">{this.props.res.authors}</div>
-            </div>
+			<li key={res.id}>
+				<div className="book">
+	              <div className="book-top">
+	                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url('+ res.imageLinks.smallThumbnail +')' }}></div>
+	                <div className="book-shelf-changer">
+	                  <select onChange={(e, r) => selectShelf(e.target.value, bookItem)}>
+	                    <option value="none">Move to...</option>
+	                    <option value="currentlyReading">Currently Reading</option>
+	                    <option value="wantToRead">Want to Read</option>
+	                    <option value="read">Read</option>
+	                    <option value="none">None</option>
+	                  </select>
+	                </div>
+	              </div>
+	              <div className="book-title">{res.title}</div>
+	              <div className="book-authors">{res.authors}</div>
+	            </div>
+            </li>
 		);
 	}
 }
